7cbd4c5a5deb5c076799e015793e5d49983f040e
Mateusz <mat.jozwiak@vp.pl>
2022-01-09 11:35:42 +0100

    More changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { AddDemotComponent } from './add-demot/add-demot.component';
 import { MainComponent } from './main/main.component';
 import { TopBarComponent} from './main/top-bar/top-bar.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { AddButtonComponent } from './add-button/add-button.component';
+import { MainTextComponent } from './main-text/main-text.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,9 @@ import { TopBarComponent} from './main/top-bar/top-bar.component';
     AddDemotComponent,
     MainComponent,
     TopBarComponent,
+    LoginButtonComponent,
+    AddButtonComponent,
+    MainTextComponent,
     
   
   ],
